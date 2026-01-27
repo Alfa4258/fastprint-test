133f26d8392b258007a8f8efad55e2d7191600e2
Alfa4258 <waterlooo42@gmail.com>
2026-01-27 20:10:24 +0700

    Setup produk app and database models

---
+++ src/produk/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Kategori(models.Model):
+    nama_kategori = models.CharField(max_length=255)
+    def __str__(self): return self.nama_kategori
+
+class Status(models.Model):
+    nama_status = models.CharField(max_length=255)
+    def __str__(self): return self.nama_status
+
+class Produk(models.Model):
+    nama_produk = models.CharField(max_length=255)
+    harga = models.DecimalField(max_digits=12, decimal_places=0)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE, related_name='produk')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='produk')
+
+    def __str__(self): return self.nama_produk
